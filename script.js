0df7bda6334d8969ca21d379c708ab62a1bb7870
Samuel Hughes <samuel971@hotmail.co.uk>
2024-05-31 10:14:23 +0100

    Create x by x divs and place them inside container

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const divContainer = document.querySelector('#container');
+
+const x = 16;
+const y =16;
+
+function createSquareElements(x, y){
+    for (let i=0; i < (x * y); i++){
+        const divSquares = document.createElement('div')
+        divSquares.className = "square-element";
+        divContainer.append(divSquares);
+    }
+}
+
+createSquareElements(x, y);
