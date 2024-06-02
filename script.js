3f8163b18dbb47eb5360b295b51555bd30d7a17b
Samuel Hughes <samuel971@hotmail.co.uk>
2024-06-02 19:10:15 +0100

    Add innerText to each div with their corresponding number/value (used as a check to make sure all divs are visible)

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const y =16;
 function createSquareElements(x, y){
     for (let i=0; i < (x * y); i++){
         const divSquares = document.createElement('div')
+        divSquares.innerText = `${i + 1}`
         divSquares.className = "square-element";
         divContainer.append(divSquares);
     }
